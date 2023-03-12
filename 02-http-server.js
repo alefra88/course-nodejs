0e87d6990fc2e5528661e02f9ca34ea8f0ccd9b5
alefra88 <efra.rlz@gmail.com>
2023-03-12 07:17:38 -0600

    add new form to say hello from nodejs

--- 02-http-server.js
+++ 02-http-server.js
@@ -10,18 +10,29 @@ http.createServer(function (request, response) {
 
 console.log('Server running at http://127.0.0.1:1337/')
 */
-'use strict'
+"use strict";
 
-var http = require('http')
+// var http = require("http");
 
-function webServer(req, res)
-{
-	res.writeHead(200, {'Content-Type':'text/html'})
-	res.end('<h1>Hola Node.js</h1>')
-}
+// function webServer(req, res) {
+//   res.writeHead(200, { "Content-Type": "text/html" });
+//   res.end("<h1>Hola Node.js</h1>");
+// }
 
-http
-	.createServer(webServer)
-	.listen(3000, 'localhost')
+// http.createServer(webServer).listen(3000, "localhost");
 
-console.log('Servidor corriendo en http://localhost:3000/')
+// console.log("Servidor corriendo en http://localhost:3000/");
+
+//new form to say hello! xd
+const http = require("http"),
+  hostname = "localhost",
+  port = 3000,
+  server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Hello World</h1>");
+  });
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
