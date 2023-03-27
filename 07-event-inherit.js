328111a906d0fc4c07a0ca64873513e48e9cb193
alefra88 <efra.rlz@gmail.com>
2023-03-27 06:30:25 -0600

    refactoring event-inherit

--- 07-event-inherit.js
+++ 07-event-inherit.js
@@ -1,35 +1,33 @@
 //https://nodejs.org/api/util.html#util_util_inherits_constructor_superconstructor
 
-'use strict'
+"use strict";
 
-var EventEmitter = require('events').EventEmitter,
-	inherits = require('util').inherits
+const EventEmitter = require("events").EventEmitter,
+  inherits = require("util").inherits;
 
-var Clock = function ()
-{
-	var self = this
+const Clock = function () {
+  const self = this;
 
-	setInterval(function (){
-		//console.log('hola')
-		self.emit('tictac')
-	}, 1000)	
-}
+  setInterval(function () {
+    //console.log('hola')
+    self.emit("tictac");
+  }, 1000);
+};
 
-inherits(Clock, EventEmitter)
+inherits(Clock, EventEmitter);
 
-Clock.prototype.theTime = function ()
-{
-	var date = new Date(),
-		hrs = date.getHours(),
-		min = date.getMinutes(),
-		sec = date.getSeconds(),
-		msg = hrs + ':' + min + ':' + sec
+Clock.prototype.theTime = function () {
+  var date = new Date(),
+    hrs = date.getHours(),
+    min = date.getMinutes(),
+    sec = date.getSeconds(),
+    msg = hrs + ":" + min + ":" + sec;
 
-	console.log(msg)
-}
+  console.log(msg);
+};
 
-var cucu = new Clock()
+const cucu = new Clock();
 
-cucu.on('tictac', function (){
-	cucu.theTime()
-})
+cucu.on("tictac", () => {
+  cucu.theTime();
+});
