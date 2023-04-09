8b84ae6d26b248ec2cab027b383dc2c17025d856
alefra88 <efra.rlz@gmail.com>
2023-04-08 22:59:29 -0600

    addd

--- 03Promesas/callback.js
+++ 03Promesas/callback.js
@@ -1,29 +1,25 @@
-'use strict'
+"use strict";
 
-const fs = require('fs'),
-	file = './assets/nombres.txt',
-	newFile = './assets/nombres-callback.txt'
+const fs = require("fs"),
+  file = "./assets/nombres.txt",
+  newFile = "./assets/nombres-callback.txt";
 
-fs.access(file, fs.F_OK, function (err){
-	if(err)
-	{
-		console.log('El archivo no existe')
-	}
-	else
-	{
-		console.log('El archivo existe')
-		fs.readFile(file, function (err, data){
-			if(err)
-			{
-				console.log('El archivo no se pudo leer')
-			}
-			else
-			{
-				console.log('El archivo se ha leído exitosamente')
-				fs.writeFile(newFile, data, function(err){
-					return (err) ? console.log('El archivo no se pudo copiar') : console.log('El archivo se ha copiado con éxito')
-				})
-			}
-		})
-	}
-})
+fs.access(file, fs.F_OK, function (err) {
+  if (err) {
+    console.log("El archivo no existe");
+  } else {
+    console.log("El archivo existe");
+    fs.readFile(file, function (err, data) {
+      if (err) {
+        console.log("El archivo no se pudo leer");
+      } else {
+        console.log("El archivo se ha leído exitosamente ");
+        fs.writeFile(newFile, data, function (err) {
+          return err
+            ? console.log("El archivo no se pudo copiar")
+            : console.log("El archivo se ha copiado con éxito");
+        });
+      }
+    });
+  }
+});
