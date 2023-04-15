2ecd77bf8eec734edce0fb9d6e5064e4f6a34994
alefra88 <efra.rlz@gmail.com>
2023-04-15 04:39:40 -0600

    hmhmhm

--- 04_express/00Basicos/00-hello-express.js
+++ 04_express/00Basicos/00-hello-express.js
@@ -1,13 +1,13 @@
-'use strict'
+"use strict";
 
-const express = require('express'),
-	app = express()
+const express = require("express"),
+  app = express();
 
 app
-	.get('/', (req, res) => {
-		res.end('<h1>Hola Mundo desde Express</h1>')
-	})
-	
-	.listen(3000)
+  .get("/", (req, res) => {
+    res.end("<h1>Hola Mundo desde Express</h1>");
+  })
 
-console.log('Iniciando Express en el puerto 3000')
+  .listen(3000);
+
+console.log("Iniciando Express en el puerto 3000");
