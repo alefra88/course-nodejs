a58a0de1a3dd63b887d1b7825855923c94987bc4
alefra88 <efra.rlz@gmail.com>
2023-04-19 23:22:13 -0600

    add

--- 04_express/01ContadorVisitas/contador-visitas.js
+++ 04_express/01ContadorVisitas/contador-visitas.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const express = require('express'),
+let express = require('express'),
 	app = express(),
 	cookieParser = require('cookie-parser'),
 	cookieSession = require('cookie-session')
