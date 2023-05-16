ecccdcad8bbc4c9027cf211f1a3b47357ced8b52
alefra88 <efra.rlz@gmail.com>
2023-05-16 06:03:51 -0600

    add

--- 07ApiCRUDMySQL/app.js
+++ 07ApiCRUDMySQL/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var express = require('express'),
+let express = require('express'),
 	favicon = require('serve-favicon'),
 	bodyParser = require('body-parser'),
 	morgan = require('morgan'),
