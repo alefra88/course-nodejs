fea532fe6b474b6e32e58e1b99da275eef8afce3
alefra88 <efra.rlz@gmail.com>
2023-05-25 23:34:20 -0600

    add

--- template_engines/server.js
+++ template_engines/server.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const app = require('./app.js'),
+let app = require('./app.js'),
 	server = app.listen(app.get('port'), () => {
 		console.log(`Iniciando Express en el puerto ${app.get('port')}`)
 	})
